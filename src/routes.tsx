42ab5499580476ddb409517dd02c4c66ad73c2a1
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-12-17 16:09:14 -0300

    feat: add layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { RootLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages'
 import { Services } from './pages/services'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
-  },
-  {
-    path: '/services',
-    element: <Services />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/services',
+        element: <Services />,
+      },
+    ],
   },
 ])
