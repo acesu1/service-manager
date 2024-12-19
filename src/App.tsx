5cb931a74da1544c00b6815e1808a70259a01385
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-12-19 14:33:53 -0300

    feat: add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './index.css'
 
+import { ThemeProvider } from './components/theme-provider'
+import { ThemeToggle } from './components/theme-toggle'
+
 export function App() {
   return (
-    <>
-      hello world
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeToggle />
+    </ThemeProvider>
   )
 }
