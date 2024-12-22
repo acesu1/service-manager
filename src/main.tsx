cc156716d0e3c0d364e9081c09a92348316ce35b
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-12-22 11:55:31 -0300

    chore: setup convex and clerk

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from './app'
 
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ConvexReactClient } from 'convex/react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <App />
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   </React.StrictMode>,
 )
